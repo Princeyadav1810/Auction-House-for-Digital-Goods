037f5ba5f938e3b4e174340cb54d30e0b4eb2610
Princeyadav1810 <sprinceyadav23@gmail.com>
2025-05-20 22:32:04 +0530

    Update Auction House for Digital Goods.sol

--- contracts/Auction House for Digital Goods.sol
+++ contracts/Auction House for Digital Goods.sol
@@ -16,23 +16,19 @@ contract DigitalAuction {
         address payable highestBidder;
         uint256 endTime;
         bool ended;
+        bool cancelled;
     }
 
     mapping(uint256 => Auction) public auctions;
+    mapping(address => uint256[]) public userBids;
     uint256 public auctionCounter;
-    
+
     // Events
     event AuctionCreated(uint256 indexed auctionId, address indexed seller, string digitalItemURI, uint256 startingPrice, uint256 endTime);
     event BidPlaced(uint256 indexed auctionId, address indexed bidder, uint256 amount);
     event AuctionEnded(uint256 indexed auctionId, address indexed winner, uint256 amount);
+    event AuctionCancelled(uint256 indexed auctionId);
 
-    /**
-     * @dev Create a new auction for a digital item
-     * @param _digitalItemURI URI to the digital item (IPFS hash or other identifier)
-     * @param _digitalItemDescription Description of the digital item
-     * @param _startingPrice Starting price for the auction
-     * @param _durationInMinutes Duration of the auction in minutes
-     */
     function createAuction(
         string memory _digitalItemURI,
         string memory _digitalItemDescription,
@@ -54,55 +50,48 @@ contract DigitalAuction {
             highestBid: 0,
             highestBidder: payable(address(0)),
             endTime: endTime,
-            ended: false
+            ended: false,
+            cancelled: false
         });
 
         emit AuctionCreated(auctionId, msg.sender, _digitalItemURI, _startingPrice, endTime);
     }
 
-    /**
-     * @dev Place a bid on an auction
-     * @param _auctionId ID of the auction
-     */
     function placeBid(uint256 _auctionId) external payable {
         Auction storage auction = auctions[_auctionId];
-        
+
         require(block.timestamp < auction.endTime, "Auction has ended");
         require(!auction.ended, "Auction already finalized");
+        require(!auction.cancelled, "Auction has been cancelled");
         require(msg.sender != auction.seller, "Seller cannot bid on their own auction");
-        
+
         uint256 currentBid = msg.value;
-        
+
         if (auction.highestBid == 0) {
             require(currentBid >= auction.startingPrice, "Bid must be at least the starting price");
         } else {
             require(currentBid > auction.highestBid, "Bid must be higher than current highest bid");
-            
-            // Refund the previous highest bidder
-            auction.highestBidder.transfer(auction.highestBid);
+            auction.highestBidder.transfer(auction.highestBid); // refund previous bidder
         }
-        
+
         auction.highestBid = currentBid;
         auction.highestBidder = payable(msg.sender);
-        
+        userBids[msg.sender].push(_auctionId);
+
         emit BidPlaced(_auctionId, msg.sender, currentBid);
     }
 
-    /**
-     * @dev End an auction and transfer funds to the seller
-     * @param _auctionId ID of the auction to end
-     */
     function endAuction(uint256 _auctionId) external {
         Auction storage auction = auctions[_auctionId];
-        
+
         require(block.timestamp >= auction.endTime, "Auction has not ended yet");
         require(!auction.ended, "Auction already finalized");
+        require(!auction.cancelled, "Auction has been cancelled");
         require(msg.sender == auction.seller || msg.sender == auction.highestBidder, "Only seller or highest bidder can end auction");
-        
+
         auction.ended = true;
-        
+
         if (auction.highestBidder != address(0)) {
-            // Transfer funds to the seller
             auction.seller.transfer(auction.highestBid);
             emit AuctionEnded(_auctionId, auction.highestBidder, auction.highestBid);
         } else {
@@ -110,31 +99,14 @@ contract DigitalAuction {
         }
     }
 
-    /*
-     * @dev Get the details of an auction
-     * @param _auctionId ID of the auction
-     * @return Auction details
-     */
-    function getAuction(uint256 _auctionId) external view returns (
-        address seller,
-        string memory digitalItemURI,
-        string memory digitalItemDescription,
-        uint256 startingPrice,
-        uint256 highestBid,
-        address highestBidder,
-        uint256 endTime,
-        bool ended
-    ) {
+    function cancelAuction(uint256 _auctionId) external {
         Auction storage auction = auctions[_auctionId];
-        return (
-            auction.seller,
-            auction.digitalItemURI,
-            auction.digitalItemDescription,
-            auction.startingPrice,
-            auction.highestBid,
-            auction.highestBidder,
-            auction.endTime,
-            auction.ended
-        );
-    }
-}
+
+        require(msg.sender == auction.seller, "Only seller can cancel auction");
+        require(!auction.ended, "Cannot cancel ended auction");
+        require(auction.highestBid == 0, "Cannot cancel after receiving bids");
+
+        auction.cancelled = true;
+        auction.ended = true;
+
+        emit AuctionCancelled(
